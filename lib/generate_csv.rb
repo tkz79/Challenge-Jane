664fc0a73f84154d2a454e5fcd92e1d93490ab46
tkz79 <tomek.kulmacz@gmail.com>
2020-06-13 10:09:39 -0700

    Some initial linting and clean up

--- lib/generate_csv.rb
+++ lib/generate_csv.rb
@@ -1,26 +1,33 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
+
 require 'csv'
 
 class GenerateCSV
   attr_reader :path, :count
 
   def initialize(path:, count: 1000)
-    @path  = path
+    @path = path
     @count = count
   end
 
   def generate
-    CSV.open(path, "wb") do |csv|
-      count.times do
-        csv << [
-          [Faker::Name.first_name, Faker::Name.last_name].join(" "),
-          Faker::Internet.free_email,
-          rand(99),
-          %W(m f)[rand(1)]
-        ]
-      end
+    CSV.open(path, 'wb') do |csv|
+      count.times { csv << generate_row }
     end
+  end
+
+  private
+
+  def generate_row
+    [
+      [Faker::Name.first_name, Faker::Name.last_name].join(' '),
+      Faker::Internet.free_email,
+      rand(99),
+      %w[m f].sample
+    ]
   end
 end
--- spec/lib/csv_skills_spec.rb
+++ spec/lib/csv_skills_spec.rb
@@ -1,14 +1,20 @@
+# frozen_string_literal: true
+
 require 'csv_skills'
 
-RSpec.describe "csv_skills" do
-  it "generates a count of the domains used in email addresses" do
+RSpec.describe 'csv_skills' do
+  let(:expected_csv_contents) do
+    [
+      %w[domain count],
+      %w[yahoo.com 331],
+      %w[gmail.com 330],
+      %w[hotmail.com 339]
+    ]
+  end
+
+  it 'generates a count of the domains used in email addresses' do
     parse_file
 
-    expect(CSV.read("output.csv")).to eq([
-      ["domain", "count"],
-      ["yahoo.com", "331"],
-      ["gmail.com", "330"],
-      ["hotmail.com", "339"],
-    ])
+    expect(CSV.read('output.csv')).to eq(expected_csv_contents)
   end
 end
